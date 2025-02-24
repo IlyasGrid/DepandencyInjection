69049cb0fd47ec62302d33736c42a0c232665e6c
ilyasGrid <ilyasgrid17@gmail.com>
2025-02-24 10:48:55 +0000

    implementing the interfaces

---
+++ src/metier/MetierImpl.java
@@ -0,0 +1,23 @@
+package metier;
+
+import dao.IDao;
+
+public class MetierImpl implements IMetier {
+    private IDao dao;
+
+//    public MetierImpl(IDao dao) {
+//        this.dao = dao;
+//    }
+
+
+    public void setDao(IDao dao) {
+        this.dao = dao;
+    }
+
+    public double calcul() {
+        System.out.println("MetierImpl.calcul()");
+        double data = dao.getData();
+        double result = data * 23;
+        return result;
+    }
+}
